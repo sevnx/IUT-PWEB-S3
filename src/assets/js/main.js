a3eb18232b51d80ee2a32b12ef2f66d566c539b7
Corentin Lenclos <corentin.lenclos@gmail.com>
2023-10-20 01:19:18 +0200

    main

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -226,15 +226,15 @@
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
+    let portfolioContainer = select('.news-container');
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
         itemSelector: '.portfolio-item'
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select('#news-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#news-flters li', function(e) {
         e.preventDefault();
         portfolioFilters.forEach(function(el) {
           el.classList.remove('filter-active');
